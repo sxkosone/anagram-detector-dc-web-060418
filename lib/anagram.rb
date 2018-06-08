f176b4f0294e0f6878b8c7a5e40472be7e778d97
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-06-08 14:56:38 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+##GROUP WORK WITH OREN
+class Anagram
+    attr_accessor :word
+  
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(possible_array)
+      word_array = word.split("").sort
+      possible_array.select do |possible|
+        possible.split("").sort == word_array
+      end
+    end
+  
+  end
